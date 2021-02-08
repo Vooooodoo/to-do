4376d5f04d52e381de1dc49f32f5732ea33d338d
FusionTech <fusion@fusion.com>
2021-02-08 16:48:15 +0300

    feature: make addToDoItem function logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,19 @@
-// найдём шаблон to-do-item-template
-const toDoItemTemplate = document.querySelector('#to-do-item-template').content;
-// создадим пустого клона элемента to-do-item внутри шаблона 
-const toDoItem = toDoItemTemplate.querySelector('.to-do-item').cloneNode(true); 
-const 
+const addButton = document.querySelector('.form__button');
+const input = document.querySelector('.form__input');
+const toDoList = document.querySelector('.to-do__list');
+
+function addToDoItem() {
+  // найдём шаблон to-do-item-template
+  const toDoItemTemplate = document.querySelector('#to-do-item-template').content;
+  // создадим пустого клона элемента to-do-item внутри шаблона
+  const toDoItem = toDoItemTemplate.querySelector('.to-do-item').cloneNode(true);
+  const toDoText = toDoItem.querySelector('.to-do-item__text');      
+    
+  toDoText.textContent = input.value;
+    
+  toDoList.append(toDoItem);
+    
+  input.value = '';
+}
+
+addButton.addEventListener('click', addToDoItem);
