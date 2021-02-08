8ce056ceb0629f118c5d7159c390d73115baa41d
FusionTech <fusion@fusion.com>
2021-02-08 17:30:25 +0300

    feature: make deleteToDoItem & completeToDoItem functions logic

--- scripts/index.js
+++ scripts/index.js
@@ -16,4 +16,18 @@ function addToDoItem() {
   input.value = '';
 }
 
+function deleteToDoItem(evt) {
+  if (evt.target.classList.contains('to-do-item__button_type_delete')) {
+    evt.target.parentNode.remove();
+  }
+}
+
+function completeToDoItem(evt) {
+  if (evt.target.classList.contains('to-do-item__button_type_complete')) {        
+    evt.target.previousElementSibling.classList.add('to-do-item__text_type_complete');
+  }
+}
+
 addButton.addEventListener('click', addToDoItem);
+document.addEventListener('click', deleteToDoItem);
+document.addEventListener('click', completeToDoItem);
